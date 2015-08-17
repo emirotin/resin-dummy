63cb0c10df5bf202620e2a8950348dd62280e91c
Eugene Mirotin <emirotin@gmail.com>
2015-08-17 16:10:19 +0300

    allow customizing the logs from env vars

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
+var name = process.env.NAME || '',
+	log = '. Running'
+
+if (name) {
+	log += ' ' + name;
+}
+log += '...';
+
 var i = 0;
+
 function run() {
-	console.log((++i) + '. Running...');
+	console.log((++i) + log);
 	setTimeout(run, 10000);
 }
 
